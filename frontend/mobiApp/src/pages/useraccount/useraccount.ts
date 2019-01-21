3fa8472f39b963aaa911e79d052cc5ea1e3dd744
IndikaWedage <wedageindika89@gmail.com>
2019-01-21 12:51:32 +0530

    created successfully the user account page by loading db data

--- frontend/mobiApp/src/pages/useraccount/useraccount.ts
+++ frontend/mobiApp/src/pages/useraccount/useraccount.ts
@@ -3,7 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomewindowPage } from '../homewindow/homewindow';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
-//import { user } from './user';
+
 
 @IonicPage()
 @Component({
@@ -12,17 +12,17 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 })
 export class UseraccountPage {
 
-   users$ :FirebaseListObservable<any>;
+   users :FirebaseListObservable<any>;
+
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams,afd :AngularFireDatabase) {
-    var uName :string;
+    var uName$ :string;
     var fullname :string;
-    uName = this.navParams.get('data');
-    console.log(uName);
-    this.users$ = afd.list('/profile/'+this.uName).valueChanges();
+    uName$ = this.navParams.get('data');
+    console.log(uName$);
+    this.users = afd.list('/profile/'+uName$).valueChanges();
 
-    // this.getProfileInfo();
   }
 
   ionViewDidLoad() {
@@ -32,12 +32,6 @@ export class UseraccountPage {
     this.navCtrl.push(HomewindowPage);
   }
 
-  getProfileInfo(){
-    this.afd.list('/profile').valueChanges().subscribe(
-      data => {
-        console.log(JSON.stringify(data));
-      }
-    );
-  }
+  
 
 }
