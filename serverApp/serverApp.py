abe15412d993090d293f0fa1df2a354b16ad6059
hashila <hashila.abeywardana1@gmail.com>
2019-01-28 22:01:15 +0530

    combined codes and made the analyser

--- serverApp/serverApp.py
+++ serverApp/serverApp.py
@@ -16,25 +16,18 @@ firebase = firebase.FirebaseApplication('https://cybrogx-1543512333299.firebasei
 
 
 ########################################## firebase CRUD operations area #########################################
-'''
-def fullNameOfUser(username):
-    userName = username
-    fireUser = firebase.get('/profile/'+userName,None)
-    userKeyDicList = fireUser.keys()
-    keyValue = ""
-    for i in userKeyDicList:
-        keyValue = i
-    fullName = firebase.get('/profile/'+userName+'/'+keyValue, 'fullName')
-    return fullName # returns the full name of the given user name
 
-def getFullNameList():
-    userinfo = firebase.get('/profile/',None)
-    keyList  = userinfo.keys()
-    fullNameList = list()
-    for k in keyList:
-        ss = fullNameOfUser(k)
-        fullNameList.append(ss)
-    return fullNameList # this list use for the search api to search about the person on the internet
+def userNameList():
+    fulFireList = firebase.get('/profile/',None)
+    return fulFireList.keys()
+
+
+
+
+
+
+
+
 
 extremeList = firebase.get('/badWords/extreme',None) # list of extreme bad words
 highList = firebase.get('/badWords/high',None) # list of high bad words
@@ -60,7 +53,7 @@ def sendBad():
 
     }
     firebase.post('/badWords/bad/',bad)
-'''
+
 ######################################################################################################
 
 
@@ -71,13 +64,17 @@ apikey = "AIzaSyBDR0vmhTIlG7UntsiN0MrUjSTDcwatB6Q"
 cx = "003540993553648637127:x-4dcla6a9g"
 googleurl = "https://www.googleapis.com/customsearch/v1"
 
-parameters = {"q":"Mahinda","cx":cx,"key":apikey}
-page = requests.request("GET", googleurl, params=parameters)
-results = json.loads(page.text)
+def search(commonName):
+    parameters = {"q":commonName,"cx":cx,"key":apikey}
+    page = requests.request("GET", googleurl, params=parameters)
+    results = json.loads(page.text)
+    urlListofSearch = list()
+    lengthofitems = len(results["items"])
+    for numb in range(lengthofitems):
+        urlset = results["items"][numb]["link"]
+        urlListofSearch.append(urlset)
+    return urlListofSearch
 
-lengthofitems = len(results["items"])
-for i in range(lengthofitems):
-    print(results["items"][i]["link"])
 
 
 
@@ -85,32 +82,100 @@ for i in range(lengthofitems):
 
 
 ########################################## webscraping area of the project ################################
-'''
-html = urlopen("https://www.javatpoint.com/")
-bsObj = BeautifulSoup(html.read(),'html.parser')
-print(bsObj.p)
-'''
+
+def scraper(url):
+    page = requests.get(url)
+    # html = urlopen(url)
+    bsObj = BeautifulSoup(page.content,'html.parser')
+    return str(bsObj.body)
+
+
 ######################################################################################################
 
+######################################## analyser #######################################################
+
+
+
+def mainAnnalyser(stng):
+    countex = 0
+    counth = 0
+    countb = 0
+    for badword in extremeList:
+        if badword == None:
+            continue
+        else:
+            countex = countex + stng.count(badword)
+    for badword in highList:
+        if badword == None:
+            continue
+        else:
+            counth = counth + stng.count(badword)
+    for badword in badList:
+        if badword == None:
+            continue
+        else:
+            countb = countb + stng.count(badword)
+    fullcnt = (countex*10) + (counth*5) + countb
+    howbad = (fullcnt/len(stng.split(" ")))*100
+    return howbad
+
+
+
+#################################################################################################################
+
+######################################### All together <Major Code> ########################################################
+
+def postResult(fullName,userName):
+    searchList = search(fullName)
+    for url in searchList:
+        stng = scraper(url)
+        howbadinweb = mainAnnalyser(stng)
+        content={
+            "url" : url,
+            "howbad" : howbadinweb
+        }
+        firebase.post('/news/'+userName,content)
+
+def doItForUser(username):
+    userName = username
+    fireUser = firebase.get('/profile/'+userName,None)
+    userKeyDicList = fireUser.keys()
+    keyValue = ""
+    for i in userKeyDicList:
+        keyValue = i
+    fullName = firebase.get('/profile/'+userName+'/'+keyValue, 'fullName')
+    postResult(fullName,userName)
+
+
+def doItForAllUsers():
+    getuserNameList = userNameList()
+    for username in getuserNameList:
+        doItForUser(username)
+    print("done")
+
+doItForAllUsers()
+
+
+#################################################################################################################
 
 ######################################## button function area of the project ####################################
-'''
+
 def startBtnAction():
-    print("start button works well.....")
+    doItForAllUsers()
 
 def stopBtnAction():
     print("stop button works well.....")
 
 def addKeywordBtnAction():
     print("add keywords button works well.....")
-'''
+
 
 ######################################################################################################
 
 
 
 ######################################### interface Area of the project ##############################
-'''
+''''
 root = Tk()
 
 topFrame = Frame(root)
